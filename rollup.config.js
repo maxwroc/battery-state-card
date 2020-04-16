ba9de42a73e110dbd2aab8469ebdb9522281d0e3
maxwroc <max.chodorowski@gmail.com>
2020-04-16 19:12:47 +0100

    Initial setup for card development

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import { uglify } from 'rollup-plugin-uglify';
+import pkg from './package.json';
+
+export default function (agrs) {
+
+  const plugins = [
+    resolve(),
+    typescript()
+  ];
+
+  let sourcemapPathTransform = undefined;
+
+  if (agrs.release) {
+    plugins.push(
+      uglify({
+        compress: {}
+      })
+    );
+
+    sourcemapPathTransform = file =>
+      pkg.repository.url.replace(/.git$/, "") + "/blob/" + pkg.version + file.substr(2);
+  }
+
+  return {
+    external: ['lit-element'],
+    input: 'src/index.ts',
+    output: {
+      globals: {
+        'lit-element': "LitElement"
+      },
+      file: pkg.main,
+      format: 'iife',
+      sourcemap: true,
+      sourcemapExcludeSources: true,
+      sourcemapPathTransform: sourcemapPathTransform
+    },
+    plugins: plugins,
+  }
+};
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { LitElement, html, svg } from "lit-element"
+
+class BatteryStateCard extends LitElement {
+    constructor() {
+        super();
+    }
+}
+
+customElements.define('battery-state-card', BatteryStateCard);
