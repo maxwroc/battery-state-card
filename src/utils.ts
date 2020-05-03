27d4fee352470ede52a85a7680738f9ae04cbf89
maxwroc <max.chodorowski@gmail.com>
2020-05-03 23:03:06 +0100

    Bump version

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 
 console.info(
-    "%c BATTERY-STATE-CARD %c 1.1.1",
+    "%c BATTERY-STATE-CARD %c 1.2.0",
     "color: white; background: forestgreen; font-weight: 700;",
     "color: forestgreen; background: white; font-weight: 700;",
 );
