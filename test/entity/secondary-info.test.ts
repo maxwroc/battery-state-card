74914a94f02c3d1c7a5fcdef43e45c4db5524b1f
Max Chodorowski <max.chodorowski@gmail.com>
2021-12-23 21:11:52 +0000

    Rename

--- test/entity/secondary-info.test.ts
+++ test/entity/secondary-info.test.ts
@@ -5,9 +5,11 @@ describe("secondary info", () => {
     afterEach(testCleanUp);
 
     test("custom text", async () => {
-        const entity = await createEntityElement(getEntityConfig({ secondary_info: "my info text" }), getHassMock());
+        const entity = await createEntityElement(
+            getEntityConfig({ secondary_info: "my info text" }), 
+            getHassMock());
         const accessors = entityElements(entity);
 
         expect(accessors.secondaryInfo()).toBe("my info text");
-    })
+    });
 })
