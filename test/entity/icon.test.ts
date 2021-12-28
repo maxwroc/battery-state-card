d2e222b2c6a64d96b1a722206c35e33254c05ec6
Max Chodorowski <max.chodorowski@gmail.com>
2021-12-28 13:49:38 +0000

    Tests in a separate file

---
+++ test/entity/icon.test.ts
@@ -0,0 +1,32 @@
+import { createEntityElement, entityElements, getEntityConfig, getHassMock, testCleanUp } from "../helpers";
+
+describe("Icon", () => {
+
+    afterEach(testCleanUp);
+
+    test("static name", async () => {
+        const config = getEntityConfig();
+        config.icon = "mdi:custom-icon";
+
+        const entity = await createEntityElement(config, getHassMock());
+        const accessors = entityElements(entity);
+        
+        expect(accessors.icon()).toBe("mdi:custom-icon");
+    });
+
+    test("Name taken from friendly_name attribute", async () => {
+        const entity = await createEntityElement(getEntityConfig(), getHassMock(100, { friendly_name: "My battery" }));
+        const accessors = entityElements(entity);
+
+        expect(accessors.name()).toBe("My battery");
+    });
+
+    test("Name taken from config override", async () => {
+        const entity = await createEntityElement(
+            getEntityConfig({ name: "Name override" }), 
+            getHassMock(100, { friendly_name: "My battery" }));
+        const accessors = entityElements(entity);
+
+        expect(accessors.name()).toBe("Name override");
+    });
+})
--- test/entity/secondary-info.test.ts
+++ test/entity/secondary-info.test.ts
@@ -18,30 +18,4 @@ describe("Icon", () => {
         
         expect(accessors.icon()).toBe(expectedIcon);
     });
-
-    test("static name", async () => {
-        const config = getEntityConfig();
-        config.icon = "mdi:custom-icon";
-
-        const entity = await createEntityElement(config, getHassMock());
-        const accessors = entityElements(entity);
-        
-        expect(accessors.icon()).toBe("mdi:custom-icon");
-    });
-
-    test("Name taken from friendly_name attribute", async () => {
-        const entity = await createEntityElement(getEntityConfig(), getHassMock(100, { friendly_name: "My battery" }));
-        const accessors = entityElements(entity);
-
-        expect(accessors.name()).toBe("My battery");
-    });
-
-    test("Name taken from config override", async () => {
-        const entity = await createEntityElement(
-            getEntityConfig({ name: "Name override" }), 
-            getHassMock(100, { friendly_name: "My battery" }));
-        const accessors = entityElements(entity);
-
-        expect(accessors.name()).toBe("Name override");
-    });
 })
