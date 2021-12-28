0117076840a54c79a890241afb85260652a1ce33
Max Chodorowski <max.chodorowski@gmail.com>
2021-12-28 13:04:25 +0000

    Additional logging

--- test/entity/secondary-info.test.ts
+++ test/entity/secondary-info.test.ts
@@ -2,7 +2,16 @@ import { createEntityElement, entityElements, getEntityConfig, getHassMock, test
 
 describe("secondary info", () => {
 
+    beforeEach(() => console.log("beforeEach"));
+
+    afterEach(() => console.log("afterEach"));
+
+    beforeAll(() => console.log("beforeAll"));
+    afterAll(() => console.log("afterAll"));
+
     test("custom text", async () => {
+        console.log("test start");
         expect(2).toBe(2);
+        console.log("test end");
     });
 })
