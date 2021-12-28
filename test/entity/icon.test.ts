27f401cf3840ee9446822470a58ba1c57641ebef
Max Chodorowski <max.chodorowski@gmail.com>
2021-12-28 22:03:12 +0000

    moved all to single file

--- test/entity/icon.test.ts
+++ test/entity/icon.test.ts
@@ -14,3 +14,39 @@ test.each([
     
     expect(accessors.icon()).toBe(expectedIcon);
 });
+
+test("Secondary info custom text", async () => {
+    const entity = await createEntityElement(
+        getEntityConfig({ secondary_info: "my info text" }), 
+        getHassMock());
+    const accessors = entityElements(entity);
+
+    expect(accessors.secondaryInfo()).toBe("my info text");
+});
+
+
+test("Static name", async () => {
+    const config = getEntityConfig();
+    config.icon = "mdi:custom-icon";
+
+    const entity = await createEntityElement(config, getHassMock());
+    const accessors = entityElements(entity);
+    
+    expect(accessors.icon()).toBe("mdi:custom-icon");
+});
+
+test("Name taken from friendly_name attribute", async () => {
+    const entity = await createEntityElement(getEntityConfig(), getHassMock(100, { friendly_name: "My battery" }));
+    const accessors = entityElements(entity);
+
+    expect(accessors.name()).toBe("My battery");
+});
+
+test("Name taken from config override", async () => {
+    const entity = await createEntityElement(
+        getEntityConfig({ name: "Name override" }), 
+        getHassMock(100, { friendly_name: "My battery" }));
+    const accessors = entityElements(entity);
+
+    expect(accessors.name()).toBe("Name override");
+});
