a2068f87ef1eaed1300cfc45f893ec63c42ba00c
Max Chodorowski <max.chodorowski@gmail.com>
2021-12-30 18:09:09 +0000

    Unit override

--- test/entity/state.test.ts
+++ test/entity/state.test.ts
@@ -41,3 +41,32 @@ test.each([
     
     expect(entity.state).toBe(expectedState);
 });
+
+test("State with custom unit", async () => {
+    const hass = new HomeAssistantMock<BatteryStateEntity>();
+    const sensor = hass.addEntity("Motion sensor battery level", "80");
+    const cardElem = hass.addCard("battery-state-entity", {
+        entity: sensor.entity_id,
+        unit: "lqi"
+    });
+    
+    await cardElem.cardUpdated;
+
+    const entity = new EntityElements(cardElem);
+    
+    expect(entity.state).toBe("80 lqi");
+});
+
+test("State with string value", async () => {
+    const hass = new HomeAssistantMock<BatteryStateEntity>();
+    const sensor = hass.addEntity("Motion sensor battery level", "Charging");
+    const cardElem = hass.addCard("battery-state-entity", {
+        entity: sensor.entity_id,
+    });
+    
+    await cardElem.cardUpdated;
+
+    const entity = new EntityElements(cardElem);
+    
+    expect(entity.state).toBe("Charging");
+});
