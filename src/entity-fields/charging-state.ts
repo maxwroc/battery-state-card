964c038198a54022a085a31342a45b43813f0fe5
Max Chodorowski <max.chodorowski@gmail.com>
2022-10-07 14:49:34 +0100

    Added missing files

---
+++ src/entity-fields/charging-state.ts
@@ -0,0 +1,67 @@
+import { HomeAssistant } from "custom-card-helpers/dist/types";
+import { log, safeGetArray } from "../utils";
+
+/**
+ * Gets flag indicating charging mode
+ * @param config Entity config
+ * @param state Battery level/state
+ * @param hass HomeAssistant state object
+ * @returns Whether battery is in chargin mode
+ */
+ export const getChargingState = (config: IBatteryEntityConfig, state: string, hass?: HomeAssistant): boolean => {
+    const chargingConfig = config.charging_state;
+    if (!chargingConfig || !hass) {
+        return false;
+    }
+
+    let entityWithChargingState = hass.states[config.entity];
+
+    // check whether we should use different entity to get charging state
+    if (chargingConfig.entity_id) {
+        entityWithChargingState = hass.states[chargingConfig.entity_id]
+        if (!entityWithChargingState) {
+            log(`'charging_state' entity id (${chargingConfig.entity_id}) not found`);
+            return false;
+        }
+
+        state = entityWithChargingState.state;
+    }
+
+    const attributesLookup = safeGetArray(chargingConfig.attribute);
+    // check if we should take the state from attribute
+    if (attributesLookup.length != 0) {
+        // take first attribute name which exists on entity
+        const exisitngAttrib = attributesLookup.find(attr => getValueFromJsonPath(entityWithChargingState.attributes, attr.name) !== undefined);
+        if (exisitngAttrib) {
+            return exisitngAttrib.value !== undefined ?
+                getValueFromJsonPath(entityWithChargingState.attributes, exisitngAttrib.name) == exisitngAttrib.value :
+                true;
+        }
+        else {
+            // if there is no attribute indicating charging it means the charging state is false
+            return false;
+        }
+    }
+
+    const statesIndicatingCharging = safeGetArray(chargingConfig.state);
+
+    return statesIndicatingCharging.length == 0 ? !!state : statesIndicatingCharging.some(s => s == state);
+}
+
+/**
+ * Returns value from given object and the path
+ * @param data Data
+ * @param path JSON path
+ * @returns Value from the path
+ */
+ const getValueFromJsonPath = (data: any, path: string) => {
+    if (data === undefined) {
+        return data;
+    }
+
+    path.split(".").forEach(chunk => {
+        data = data ? data[chunk] : undefined;
+    });
+
+    return data;
+}
